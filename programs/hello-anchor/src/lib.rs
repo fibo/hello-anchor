25e1dde03fb970d57b355046314b89099e5dfb82
fibo <casati_gianluca@yahoo.it>
2022-03-17 14:39:06 +0100

    can count gifs

--- programs/hello-anchor/src/lib.rs
+++ programs/hello-anchor/src/lib.rs
@@ -7,9 +7,26 @@ pub mod hello_anchor {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
         Ok(())
     }
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- tests/hello-anchor.js
+++ tests/hello-anchor.js
@@ -1,13 +1,35 @@
 const anchor = require("@project-serum/anchor");
 
+const { SystemProgram } = anchor.web3;
+
 describe("hello-anchor", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   it("Is initialized!", async () => {
-    // Add your test here.
+    // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+    const provider = anchor.Provider.env();
+    anchor.setProvider(provider);
+
     const program = anchor.workspace.HelloAnchor;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+
+    // Create an account keypair for our program to use.
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    const tx = await program.rpc.initialize({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+    console.info("Your transaction signature", tx);
+
+    // Fetch data from the account.
+    let account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+    console.info("👀 GIF Count", account.totalGifs.toString());
   });
 });
